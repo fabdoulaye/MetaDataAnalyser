31fadf2eb9ffa065739facee47fecbf6262a684c
fabdoulaye <73080697+fabdoulaye@users.noreply.github.com>
2023-11-02 14:55:41 +0100

    Ajout des fonctions pour les fichiers docx et images
    ajoute de quelques propriétés

--- getFileMetaData.py
+++ getFileMetaData.py
@@ -1,10 +1,16 @@
 # getFileMetaData
 # Pour extraire et afficher toutes les métadonnées d'un fichier PDF
 from PyPDF2 import PdfReader
+# Importez le module pillow
+from PIL import Image, ExifTags
+import docx2txt
+import os
+
 
 monPdF = "C:/Users/afadi/Documents/MS EFC/intro_formation.pdf"
 
 def extractPDFMeta(chemin_du_pdf):
+
     # Ouvrez le fichier PDF en mode lecture
     reader = PdfReader(chemin_du_pdf)
 
@@ -24,4 +30,100 @@ def extractPDFMeta(chemin_du_pdf):
     #    print("Le fichier PDF est chiffré et nécessite un mot de passe pour accéder aux métadonnées.")
     #else:
 
-extractPDFMeta(monPdF)
+def extractImgMeta(chemin_de_img):
+
+    # Chargez l'image
+    img = Image.open(chemin_de_img)
+
+    # Obtenez les métadonnées
+    exif = img.getexif()
+
+    # Convertissez les métadonnées en une forme lisible par l'homme
+    for tag_id in exif:
+        # Obtenez le nom du tag
+        tag = ExifTags.TAGS.get(tag_id, tag_id)
+        # Obtenez la valeur du tag
+        value = exif.get(tag_id)
+        # Si la valeur est une séquence d'octets, décodez-la
+        if isinstance(value, bytes):
+            value = value.decode()
+        # Imprimez le nom et la valeur du tag
+        print(f"{tag:25}: {value}")
+
+
+
+
+# requirement : pip install doc2txt
+def extractDOCmetadata(file_path):
+    # Créez une liste pour stocker les métadonnées de tous les fichiers DOCX
+    all_metadata = []
+
+    try:
+        # """ # Parcourez le répertoire spécifié
+        # for root, dirs, files in os.walk(folder_path):
+        #     for file in files:
+        #         if file.lower().endswith(".docx"):  # Vérifiez si le fichier est un DOCX
+        #             file_path = os.path.join(root, file) """
+
+        # Utilisez la bibliothèque docx2txt pour extraire les métadonnées
+        doc_data = docx2txt.process(file_path)
+
+        # Extrait les métadonnées du document Word
+        metadata = {}
+        if "title" in doc_data:
+            metadata["Titre"] = doc_data["title"]
+        else:
+            metadata["Titre"] = "N/A"
+        if "created" in doc_data:
+            metadata["Date de Création"] = doc_data["created"]
+        else:
+            metadata["Date de Création"] = "N/A"
+        if "modified" in doc_data:
+            metadata["Date de Modification"] = doc_data["modified"]
+        else:
+            metadata["Date de Modification"] = "N/A"
+        if "author" in doc_data:
+            metadata["Propriétaire"] = doc_data["author"]
+        else:
+            metadata["Propriétaire"] = "N/A"
+            metadata["Nom du Document"] = os.path.basename(file_path)
+            metadata["Type de Document"] = "Microsoft Word (.docx)"
+            metadata["Taille du Document"] = os.path.getsize(file_path)
+            metadata["Nom de l'Ordinateur"] = platform.node()
+
+        # entrer le code pour les autres fichiers ici
+
+        all_metadata.append(metadata)
+
+        # Affichez les métadonnées extraites pour chaque fichier
+        for metadata in all_metadata:
+            print("Métadonnées du fichier :")
+            for key, value in metadata.items():
+                print(f"{key}: {value}")
+            print("\n")
+
+        return all_metadata
+
+    except Exception as e:
+        print(f"Erreur lors de l'extraction des métadonnées : {str(e)}")
+        return []
+
+# # Spécifiez le chemin du répertoire à parcourir
+folder_to_scan = "C:/Users/afadi/Documents/MS EFC/_1009_UE3_/TP2.docx"
+
+# # Appelez la fonction pour extraire les métadonnées des fichiers DOCX dans le répertoire
+metadata_list = extractDOCmetadata(folder_to_scan)
+
+
+    # # Affichez les métadonnées extraites pour chaque fichier
+    # for metadata in metadata_list:
+    #     print("Métadonnées du fichier :")
+    #     for key, value in metadata.items():
+    #         print(f"{key}: {value}")
+    #     print("\n")
+
+#monPdF = "C:/Users/afadi/Documents/MS EFC/intro_formation.pdf"
+#extractPDFMeta(monPdF)
+
+#monIMG = "C:/Windows/SystemApps/MicrosoftWindows.Client.CBS_cw5n1h2txyewy/DesktopSpotlight/Assets/Images/image_3.jpg"
+#extractImgMeta(monIMG)
--- listeFichiers.py
+++ listeFichiers.py
@@ -1,8 +1,12 @@
 import os
+from datetime import datetime
 import getFileMetaData
+import platform
 
 # Spécifiez le chemin du répertoire que vous souhaitez lister
 repertoire = "C:/Users/afadi/Documents/MS EFC"
+
+nom_ordinateur = platform.node()
 
 # Utilisez os.listdir() pour obtenir la liste des fichiers dans le répertoire
 fichiers = os.listdir(repertoire)
@@ -16,15 +20,39 @@ for fichier in fichiers:
 
     # Vérifiez si le chemin correspond à un fichier
     if os.path.isfile(chemin_du_fichier):
+
+        # Extensions d'images courantes
+        image_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.gif', '.tiff', '.webp', '.ico', 'svg', '.ai', '.eps', '.psd', '.heic']  
+
+    
         print(f"{chemin_du_fichier} est un fichier.")
         
          # Obtenez les informations sur le fichier
         infos_fichier = os.stat(chemin_du_fichier)
     
+        # Récupération des metadonnées
+        file_name = os.path.basename(chemin_du_fichier)
+        file_size = infos_fichier.st_size
+        created_datetime = datetime.utcfromtimestamp(infos_fichier.st_ctime)
+        modified_datetime = datetime.utcfromtimestamp(infos_fichier.st_mtime)
+        accessed_datetime = datetime.utcfromtimestamp(infos_fichier.st_atime)
+        file_owner = infos_fichier.st_uid
+        file_permissions = oct(infos_fichier.st_mode & 0o777)
+        file_path = os.path.abspath(chemin_du_fichier) # Chemin d'accès
+
+
         # Affichez les métadonnées de base
-        print("Nom du fichier:", os.path.basename(chemin_du_fichier))
-        print("Taille du fichier en octets:", infos_fichier.st_size)
-        print("Date de modification (timestamp):", infos_fichier.st_mtime)
+        print("Nom du fichier:", file_name)
+        print("Taille du fichier en octets:", file_size)
+        print("Date de création:", created_datetime)
+        print("Date de modification:", modified_datetime)
+        print("Date d'accès:", accessed_datetime)
+        print("Propriétaire du fichier:", file_owner)
+        print("Permissions de fichier:",file_permissions)
+        print("Chemin d'accès", file_path)
+        print("Nom de l'Ordinateur", nom_ordinateur)
+        
+        #print("Date de création formatée:", created_timestamp.isoformat())
     
         # Utilisez os.path.splitext() pour obtenir l'extension
         nom_base, extension = os.path.splitext(chemin_du_fichier)
@@ -32,7 +60,12 @@ for fichier in fichiers:
 
         if extension == '.pdf':
             getFileMetaData.extractPDFMeta(chemin_du_fichier)
+        elif extension in image_extensions:
+            getFileMetaData.extractImgMeta(chemin_du_fichier)
+        elif extension == '.docx':
+            getFileMetaData.extractDOCmetadata(chemin_du_fichier)
             
+        print(25*'-')
 
     else:
         print(f"{chemin_du_fichier} n'est pas un fichier.")
