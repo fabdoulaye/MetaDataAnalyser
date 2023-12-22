1c5a1c1ed3ebe902c5387e7051957e9e9ee16932
fabdoulaye <73080697+fabdoulaye@users.noreply.github.com>
2023-12-22 14:49:56 +0100

    ajout empreinte & recup metadata pdf

--- getFileMetaData.py
+++ getFileMetaData.py
@@ -5,12 +5,14 @@ from PyPDF2 import PdfReader
 from PIL import Image, ExifTags
 #import docx2txt
 from docx import Document
+# Pour utiliser PowerShell pour exécuter une commande
+import subprocess 
 
 
 monPdF = "C:/Users/afadi/Documents/MS EFC/intro_formation.pdf"
 
 def extractPDFMeta(chemin_du_pdf):
-
+    
     # Ouvrez le fichier PDF en mode lecture
     reader = PdfReader(chemin_du_pdf)
 
@@ -24,6 +26,16 @@ def extractPDFMeta(chemin_du_pdf):
     print("Titre:", meta.title)
     print("Producteur:", meta.producer)
     print("Nombre de pages:", len(reader.pages))
+    
+    return {"Chemin": chemin_du_pdf ,
+                        "Auteur":meta.author,
+                        "Créateur":meta.creator,
+                        "Sujet": meta.subject ,
+                        "Titre": meta.title ,
+                        "Producteur": meta.producer ,
+                        "Nombre de pages": len(reader.pages)
+                        }    
+    #print("Contenu de la liste :", metadataPDF)    
     
     # Vérifiez si le fichier PDF est chiffré (protégé par un mot de passe)
     #if pdf.is_encrypted:
@@ -49,6 +61,9 @@ def extractImgMeta(chemin_de_img):
             value = value.decode()
         # Imprimez le nom et la valeur du tag
         print(f"{tag:25}: {value}")
+        
+    # Créez un DataFrame à partir de la liste de métadonnées
+    #df_metadonnees = pd.DataFrame(metadonnees)
 
 
 def extractDOCXMeta(chemin_du_doc):
@@ -102,3 +117,26 @@ def extractDOCXMeta(chemin_du_doc):
 
 #monIMG = "C:/Windows/SystemApps/MicrosoftWindows.Client.CBS_cw5n1h2txyewy/DesktopSpotlight/Assets/Images/image_3.jpg"
 #extractImgMeta(monIMG)
+
+def calculer_md5(chemin_fichier):
+    
+    commande_powershell = f"Get-FileHash -Algorithm MD5 -Path '{chemin_fichier}' | Select-Object -ExpandProperty Hash"
+    
+    process = subprocess.Popen(["powershell", "-Command", commande_powershell], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    sortie, erreur = process.communicate()
+    
+    if process.returncode == 0:
+        empreinte_md5 = sortie.strip().decode('utf-8')
+        #♣print("Empreinte MD5 du fichier :", empreinte_md5)
+    else:
+        empreinte_md5 = erreur.decode('utf-8')
+        #print("Erreur lors du calcul du hash :", erreur.decode('utf-8'))
+    return empreinte_md5
+
+
+def trouver_ligne_par_valeur(df, nom_colonne, valeur):
+    ligne = df[df[nom_colonne] == valeur]
+    if not ligne.empty:
+        return ligne.iloc[0]  # Renvoie la première ligne correspondante
+    else:
+        return None
--- listeFichiers.py
+++ listeFichiers.py
@@ -5,156 +5,156 @@ import platform
 import pandas as pd
 import magic
 import stat
+import win32security
 # importing pwd module  
 #import pwd 
 
 
 # Spécifiez le chemin du répertoire que vous souhaitez lister
-repertoire = "C:/Users/hp/Documents/MSEFC/Pentest"
+repertoire = "C:/Users/hp/Documents/MSEFC/Stage" #"C:/Users/hp/Documents/MSEFC/Pentest" #"C:\Program Files (x86)" #"C:/Users/hp/Documents/MSEFC" #"Pentest"
 
 # Nom de l'ordinateur sur lequel le script tourne
 nom_ordinateur = platform.node()
 
+# Mapping des valeurs de la variable aux fonctions correspondantes
+fonctions = {
+    ".pdf" : getFileMetaData.extractPDFMeta,
+    2: getFileMetaData.extractImgMeta ,
+    3: getFileMetaData.extractDOCXMeta ,
+}
+
 # Liste pour stocker les métadonnées des fichiers
 metadonnees = []
+# Liste pour stocker les métadonnées des fichiers PDF
+metadataPDF = []
+
 
 for racine, sous_repertoires, fichiers in os.walk(repertoire):
-        for fichier in fichiers:
-            chemin_complet = os.path.join(racine, fichier)
-            nom_fichier, extension = os.path.splitext(chemin_complet)
-            # Les fichiers cachés
-            fichier_cache = True if fichier.startswith('.') else False
-            # Return the size, in bytes, of path
-            taille = os.path.getsize(chemin_complet)  # Taille en octets
-            # Return the time of last modification of path. (timestamp)
-            modified_datetime = datetime.utcfromtimestamp(os.path.getmtime(chemin_complet))
-            # Return the time of last access of path
-            accessed_datetime = datetime.utcfromtimestamp(os.path.getatime(chemin_complet))
-            # Return the system's ctime which, on some systems (like Unix) is the time of the last metadata change, and, on others (like Windows), is the creation time for path
-            created_datetime = datetime.utcfromtimestamp(os.path.getctime(chemin_complet))
-            # Return True if pathname path is a mount point: a point in a file system where a different file system has been mounted
-            mount_point = os.path.ismount(chemin_complet)
-            # Renvoie une version absolue et normalisée du chemin d'accès path
-            absolute_path = os.path.abspath(chemin_complet)
-            
-            # Récupérer les informations du propriétaire (sous Unix)
-            infos_fichier = os.stat(chemin_complet)
-            #proprietaire = pwd.getpwuid(infos_fichier.st_uid).pw_name
-            file_owner = infos_fichier.st_uid
-            # Obtenez les autorisations du fichier (permissions)
-            autorisations = stat.S_IMODE(infos_fichier.st_mode)
-            # Obtenez les autorisations du propriétaire du fichier (permissions)
-            autorisations_proprietaire = infos_fichier.st_mode & 0o700  # Masque pour les autorisations du propriétaire
+    for fichier in fichiers:
+        chemin_complet = os.path.join(racine, fichier)
+        nom_fichier, extension = os.path.splitext(chemin_complet)
+        # Les fichiers cachés
+        fichier_cache = True if fichier.startswith('.') else False
+        # Return the size, in bytes, of path
+        taille = os.path.getsize(chemin_complet)  # Taille en octets
+        # Return the time of last modification of path. (timestamp)
+        modified_datetime = datetime.utcfromtimestamp(os.path.getmtime(chemin_complet))
+        # Return the time of last access of path
+        accessed_datetime = datetime.utcfromtimestamp(os.path.getatime(chemin_complet))
+        # Return the system's ctime which, on some systems (like Unix) is the time of the last metadata change, and, on others (like Windows), is the creation time for path
+        created_datetime = datetime.utcfromtimestamp(os.path.getctime(chemin_complet))
+        # Return True if pathname path is a mount point: a point in a file system where a different file system has been mounted
+        mount_point = os.path.ismount(chemin_complet)
+        # Renvoie une version absolue et normalisée du chemin d'accès path
+        absolute_path = os.path.abspath(chemin_complet)
+        
+# =============================================================================
+#             # Récupérer les informations du propriétaire (sous Unix)
+#             infos_fichier = os.stat(chemin_complet)
+#             proprietaire = pwd.getpwuid(infos_fichier.st_uid).pw_name
+#             file_owner = infos_fichier.st_uid
+# =============================================================================
+        
+        # Récupérer les informations du propriétaire (sous Unix)
+        infos_fichier = os.stat(chemin_complet)
 
-            # Affichage des autorisations séparément pour le propriétaire du fichier
-            lecture_proprietaire = autorisations_proprietaire & stat.S_IRUSR != 0
-            ecriture_proprietaire = autorisations_proprietaire & stat.S_IWUSR != 0
-            execution_proprietaire = autorisations_proprietaire & stat.S_IXUSR != 0
-            
-            
-            # Créez un objet Magic pour accéder aux fonctionnalités de détection de type de fichier
-            mime = magic.Magic()
-            # Utilisez la méthode `from_file` pour déterminer le type de fichier
-            file_type = mime.from_file(chemin_complet)
+        # Obtenez les autorisations du fichier (permissions)
+        autorisations = stat.S_IMODE(infos_fichier.st_mode)
+        # Obtenez les autorisations du propriétaire du fichier (permissions)
+        autorisations_proprietaire = infos_fichier.st_mode & 0o700  # Masque pour les autorisations du propriétaire
+
+        # Affichage des autorisations séparément pour le propriétaire du fichier
+        lecture_proprietaire = autorisations_proprietaire & stat.S_IRUSR != 0
+        ecriture_proprietaire = autorisations_proprietaire & stat.S_IWUSR != 0
+        execution_proprietaire = autorisations_proprietaire & stat.S_IXUSR != 0
+        # Affichage des autorisations séparément pour le groupe du fichier
+        lecture_groupe = autorisations & stat.S_IRGRP != 0
+        ecriture_groupe = autorisations & stat.S_IWGRP != 0
+        execution_groupe = autorisations & stat.S_IXGRP != 0
+        # Affichage des autorisations séparément pour les autres utilisateurs du fichier
+        lecture_autres = autorisations & stat.S_IROTH != 0
+        ecriture_autres = autorisations & stat.S_IWOTH != 0
+        execution_autres = autorisations & stat.S_IXOTH != 0
+        
+        
+        
+        # Récupérer les informations du propriétaire (sous Windows)
+        infos_fichier = win32security.GetFileSecurity(chemin_complet, win32security.OWNER_SECURITY_INFORMATION)
+        proprietaire_sid = infos_fichier.GetSecurityDescriptorOwner()
+        proprietaire_nom, _, _ = win32security.LookupAccountSid(None, proprietaire_sid)
+        
+        
+        # Créez un objet Magic pour accéder aux fonctionnalités de détection de type de fichier
+        mime = magic.Magic()
+        # Utilisez la méthode `from_file` pour déterminer le type de fichier
+        file_type = mime.from_file(chemin_complet)
+        
+        # Système de fichiers
+        #infos_fs = os.statvfs(chemin_complet)
+        #type_fs = infos_fs.f_basetype
+        
+        empreinte_md5 = getFileMetaData.calculer_md5(chemin_complet)
+        
+        # Specific Meta data
+        SpecificMetaData = True
+        # Vérifier si la valeur existe dans le dictionnaire et exécuter la fonction correspondante
+        if extension in fonctions:
+            metadataPDF.append(fonctions[extension](chemin_complet))
+        else:
+            SpecificMetaData = False
+            #print("Aucune fonction associée à cette valeur")
 
 
-            # Ajoutez les métadonnées à la liste
-            metadonnees.append({
-                "Nom du fichier": nom_fichier,
-                "Chemin complet": chemin_complet,
-                "Fichier caché": fichier_cache,
-                "Taille (octets)": taille,
-                "Date de création": created_datetime,
-                "Date de modification": modified_datetime,
-                "Date du dernier accès": accessed_datetime,
-                "Point de montage": mount_point,
-                "Chemin absolue": absolute_path,
-                "Propriétaire du fichier": file_owner,
-                "Type MIME": file_type,
-                "Extension": extension,
-                "Permissions de fichier":autorisations,
-                "Propriétaire Lecture" : lecture_proprietaire,
-                "Propriétaire Ecriture" : ecriture_proprietaire ,
-                "Propriétaire Exécution" : execution_proprietaire ,
-                #"Nom Propriétaire du fichier": proprietaire,
-           })
+        # Ajoutez les métadonnées à la liste
+        metadonnees.append({
+            "Nom du fichier": fichier,
+            "Chemin complet": chemin_complet,
+            "Fichier caché": fichier_cache,
+            "Taille (octets)": taille,
+            "Date de création": created_datetime,
+            "Date de modification": modified_datetime,
+            "Date du dernier accès": accessed_datetime,
+            "Point de montage": mount_point,
+            "Chemin absolue": absolute_path,
+            "Propriétaire du fichier": proprietaire_nom, #file_owner,
+            "Type MIME": file_type,
+            "Extension": extension,
+            "Permissions de fichier": autorisations,
+            "Propriétaire Lecture" : lecture_proprietaire,
+            "Propriétaire Ecriture" : ecriture_proprietaire ,
+            "Propriétaire Exécution" : execution_proprietaire ,
+            "Groupe Lecture" : lecture_groupe ,
+            "Groupe Ecriture" : ecriture_groupe ,
+            "Groupe Exécution" : execution_groupe ,
+            "Autres Lecture" : lecture_autres ,
+            "Autres Ecriture" : ecriture_autres ,
+            "Autres Exécution" : execution_autres,
+            "Hash MD5" : empreinte_md5,
+            #"Système de fichiers" : type_fs
+       })
                 
 # Créez un DataFrame à partir de la liste de métadonnées
 df_metadonnees = pd.DataFrame(metadonnees)
 
+# Créez un DataFrame à partir de la liste de métadonnées
+df_metadataPDF = pd.DataFrame(metadataPDF)
+
 # Affichez le DataFrame
-print(df_metadonnees[["Date de création"]])
+ #print(df_metadonnees[["Date de création"]])
 print("Nombre de fichiers", df_metadonnees.shape[0])
 print(df_metadonnees)
+print(25*'-')
+print(df_metadataPDF )
 
 # Enregistrement du DataFrame dans un fichier Excel
-nom_fichier = "Metadonnees"  # Nom du fichier Excel
+nom_fichier = "Metadonnees.csv"  # Nom du fichier Excel
 df_metadonnees.to_csv(nom_fichier)  # index=False pour ne pas inclure les index dans le fichier Excel
 
-# =============================================================================
-# # Utilisez os.listdir() pour obtenir la liste des fichiers dans le répertoire
-# fichiers = os.listdir(repertoire)
-# 
-# # Parcourez la liste de fichiers et affichez-les un par un
-# for fichier in fichiers:
-#     print(fichier)
-#     chemin_du_fichier = repertoire + '/' + fichier
-#     # Vérifiez si le fichier existe
-#     #if os.path.exists(chemin_du_fichier):
-# 
-#     # Vérifiez si le chemin correspond à un fichier
-#     if os.path.isfile(chemin_du_fichier):
-# 
-#         # Extensions d'images courantes
-#         image_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.gif', '.tiff', '.webp', '.ico', 'svg', '.ai', '.eps', '.psd', '.heic']  
-# 
-#     
-#         print(f"{chemin_du_fichier} est un fichier.")
-#         
-#          # Obtenez les informations sur le fichier
-#         infos_fichier = os.stat(chemin_du_fichier)
-#     
-#         # Récupération des metadonnées
-#         file_name = os.path.basename(chemin_du_fichier)
-#         file_size = infos_fichier.st_size
-#         created_datetime = datetime.utcfromtimestamp(infos_fichier.st_ctime)
-#         modified_datetime = datetime.utcfromtimestamp(infos_fichier.st_mtime)
-#         accessed_datetime = datetime.utcfromtimestamp(infos_fichier.st_atime)
-#         file_owner = infos_fichier.st_uid
-#         file_permissions = oct(infos_fichier.st_mode & 0o777)
-#         file_path = os.path.abspath(chemin_du_fichier) # Chemin d'accès
-# 
-# 
-#         # Affichez les métadonnées de base
-#         print("Nom du fichier:", file_name)
-#         print("Taille du fichier en octets:", file_size)
-#         print("Date de création:", created_datetime)
-#         print("Date de modification:", modified_datetime)
-#         print("Date d'accès:", accessed_datetime)
-#         print("Propriétaire du fichier:", file_owner)
-#         print("Permissions de fichier:",file_permissions)
-#         print("Chemin d'accès", file_path)
-#         print("Nom de l'Ordinateur", nom_ordinateur)
-#         
-#         #print("Date de création formatée:", created_timestamp.isoformat())
-#     
-#         # Utilisez os.path.splitext() pour obtenir l'extension
-#         nom_base, extension = os.path.splitext(chemin_du_fichier)
-#         print("Extension du fichier:", extension)
-# 
-#         print(25*'*')
-#         if extension == '.docx':
-#             getFileMetaData.extractDOCXMeta(chemin_du_fichier)
-#         elif extension == '.pdf':
-#             getFileMetaData.extractPDFMeta(chemin_du_fichier)
-#         elif extension in image_extensions:
-#             getFileMetaData.extractImgMeta(chemin_du_fichier)
-#             
-#         print(25*'-')
-# 
-#     else:
-#         print(f"{chemin_du_fichier} n'est pas un fichier.")
-# =============================================================================
-        
-    #else:
-    #    print("Le fichier n'existe pas.")
+
+print(25*'-')
+# Utilisation de la fonction pour trouver une ligne par valeur dans une colonne
+resultat = getFileMetaData.trouver_ligne_par_valeur(df_metadataPDF, 'Chemin', 'C:/Users/hp/Documents/MSEFC/Stage\offre_stage_pentest_2023-2024_v2.pdf')
+if resultat is not None:
+    print("Ligne correspondante :", resultat)
+else:
+    print("Aucune correspondance trouvée.")
